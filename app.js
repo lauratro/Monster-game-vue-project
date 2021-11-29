f23840b4d5cda7565911933ea9aa9310cb1acc6a
Laura <65158950+lauratro@users.noreply.github.com>
2021-11-29 15:46:27 +0100

    restart game func

--- app.js
+++ app.js
@@ -29,9 +29,15 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyle() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyle() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     currentRoundCalc() {
@@ -39,6 +45,12 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
